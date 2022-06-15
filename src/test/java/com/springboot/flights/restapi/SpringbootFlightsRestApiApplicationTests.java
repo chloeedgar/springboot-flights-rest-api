190f20594524a6ab1b65d985546f1c5d512eb8b2
Chloe Edgar <chloeedgar1996@hotmail.co.uk>
2022-06-15 21:04:06 +0100

    first commit

---
+++ src/test/java/com/springboot/flights/restapi/SpringbootFlightsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.flights.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFlightsRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
